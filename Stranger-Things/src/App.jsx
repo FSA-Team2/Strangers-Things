c53b8f989d552acc541bae30e23e9a99475dd675
Catherine Hilton <126924004+cat-thebeesknees@users.noreply.github.com>
2023-08-09 00:42:09 -0400

    got the api working to fetch exisiting users, set up token authenticate, and Sign up form to add a new user aka register. Recycled some code from last workshop. Still lots to do but a start at least!

--- Stranger-Things/src/App.jsx
+++ Stranger-Things/src/App.jsx
@@ -1,14 +1,28 @@
 import { useState } from 'react'
 import './App.css'
-
+import FetchPosts from './API-Source/API'
+import Authenticate from './components/Authenticate';
+import SignUpForm from './components/NewUserSignUp';
 function App() {
-  // const [count, setCount] = useState(0)
+  const [token, setToken] = useState(null);
 
   return (
     <>
+    <div id='conatiner'>
+    <FetchPosts />
+    <SignUpForm token={token} setToken={setToken} />
+    <Authenticate token={token} />
+
+    </div>
+
       
     </>
-  )
+  );
+ 
 }
+
+Authenticate.defaultProps = {
+  token: null,
+};
 
 export default App
