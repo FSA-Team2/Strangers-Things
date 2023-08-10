0f8927038e7d467ba05a15b31ca7e47101d84675
Catherine Hilton <126924004+cat-thebeesknees@users.noreply.github.com>
2023-08-10 01:17:52 -0400

    updated all components, NavBar is complete with the data so far, as well as Routes, Home, API. Fetch works for api endpoint posts without any errors displayed in the console.

--- Stranger-Things/src/API-Source/API.js
+++ Stranger-Things/src/API-Source/API.js
@@ -2,25 +2,39 @@ const COHORT_NAME = '2302-ACC-PT-WEB-PT-A';
 const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`;
 
 //fetch posts
-export const FetchPosts = async () => {
-    try {
-        const response = await fetch(`${BASE_URL}/posts`, {
-            headers: {
-                'Content-Type': 'application/json',
+
+
+
+export async function fetchPosts() {
+  try {
+    const response = await fetch(`${BASE_URL}/posts`);
+    const responseJson = await response.json();
+    const posts = responseJson.data.posts;
+    console.log(posts);
+    return posts;
+  }catch (error) {
+    console.error(error);
+  }
+}
+// export const FetchPosts = async () => {
+//     try {
+//         const response = await fetch(`${BASE_URL}/posts`, {
+//             headers: {
+//                 'Content-Type': 'application/json',
                 
-              },
-            });
-            const responseJson = await response.json();
-            // console.log(responseJson);
-            const allPosts = responseJson.data.posts;
-            console.log(allPosts);
-            return allPosts;
+//               },
+//             });
+//             const responseJson = await response.json();
+//             // console.log(responseJson);
+//             const allPosts = responseJson.data.posts;
+//             console.log(allPosts);
+//             return allPosts;
             
-          } catch (err) {
-            console.error(err);
-          }
-        }
+//           } catch (err) {
+//             console.error(err);
+//           }
+//         }
 
 
 
-export default FetchPosts;
+export default fetchPosts;
--- Stranger-Things/src/App.jsx
+++ Stranger-Things/src/App.jsx
@@ -1,28 +1,31 @@
-import { useState } from 'react'
-import './App.css'
-import FetchPosts from './API-Source/API'
-import Authenticate from './components/Authenticate';
-import SignUpForm from './components/NewUserSignUp';
+import { useState } from "react";
+import "./App.css";
+import "./API-Source/API";
+import Authenticate from "./components/Authenticate";
+import SignUpForm from "./components/NewUserSignUp";
+import Navbar from "./components/NavBar";
+import RoutePaths from "./components/RoutesPaths";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Home } from "./components/HomePage";
+
 function App() {
   const [token, setToken] = useState(null);
 
   return (
     <>
-    <div id='conatiner'>
-    <FetchPosts />
-    <SignUpForm token={token} setToken={setToken} />
-    <Authenticate token={token} />
-
-    </div>
-
-      
+      <div id="conatiner">
+        <Home />
+        <RoutePaths />
+        <Navbar />
+        <SignUpForm token={token} setToken={setToken} />
+        <Authenticate token={token} />
+      </div>
     </>
   );
- 
 }
 
 Authenticate.defaultProps = {
   token: null,
 };
 
-export default App
+export default App;
