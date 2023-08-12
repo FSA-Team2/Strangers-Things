e87b9c603b388ca24ceabf0c3a52580cfc59db97
Mohamed Faki <hammadi.faki@gmail.com>
2023-08-12 02:11:32 -0400

    add post styling

--- Stranger-Things/src/components/Posts.jsx
+++ Stranger-Things/src/components/Posts.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { fetchPosts } from "../API-Source/API";
 import "../App.css";
-
+import '../components/Posts.css'
 
 
 export default function Posts() {
@@ -22,16 +22,16 @@ export default function Posts() {
   }, []);
 
   return (
-    <div>
+    <div className="posts-container">
       <h1>Posts</h1>
       <ul>
         {posts.map((post) => (
-          <li key={post.id}>
-            <h2>{post.title}</h2>
-            <p>{post.description}</p>
-            <p>price: {post.price}</p>
-            <p>location: {post.location}</p>
-            <p>will deliver: {post.willDeliver}</p>
+          <li key={post.id} className="post-item">
+            <h2 className="post-title">{post.title}</h2>
+            <p className="post-description">{post.description}</p>
+            <p className="post-price">price: {post.price}</p>
+            <p className="post.location">location: {post.location}</p>
+            <p className="post-delivery">will deliver: {post.willDeliver}</p>
           </li>
         ))}
       </ul>
